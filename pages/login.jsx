2b5e2400a30d6e2a0d435fea40e5adab760ccdb8
Gabriel Costa <22gabrielgc@gmail.com>
2023-04-03 12:18:27 -0300

    Criado componente de Imagem

---
+++ pages/login.jsx
@@ -0,0 +1,12 @@
+import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+
+
+function LoginPage () {
+    return (
+        <ImageWithSpace>
+            Teste
+        </ImageWithSpace>
+    )
+}
+
+export default LoginPage
