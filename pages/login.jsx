5dce7fa6302c4ce08ba1b18a99ab948f72fee1fb
Gabriel Costa <22gabrielgc@gmail.com>
2023-04-04 11:57:17 -0300

    Criado componentes e Tema

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,6 +4,8 @@ import ImageWithSpace from "../src/components/layout/ImageWithSpace"
 import H1 from "../src/components/typography/H1"
 import H2 from "../src/components/typography/H2"
 import H4 from "../src/components/typography/H4"
+import Button from "../src/components/inputs/button"
+import Input from "../src/components/inputs/Input"
 
 const FormContainer = styled.div `
     margin-top: 60px;
@@ -29,9 +31,9 @@ function LoginPage () {
             <FormContainer>
                 <H2>Entre em sua conta</H2>
                 <Form>
-                    <input placeholder="Email ou usuário" type="email"/>
-                    <input placeholder="Senha" type="password"/>
-                    <button>Entrar</button>
+                    <Input label="Email ou usuário" type="email"/>
+                    <Input label="Senha" type="password"/>
+                    <Button>Entrar</Button>
                 </Form>
                 <Text>Não possui uma conta? <a href="#">Faça seu cadastro</a></Text>
             </FormContainer>
