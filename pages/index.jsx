d07b39165a6039446edb7c7e8ab37768fb8cc8f1
Gabriel Costa <22gabrielgc@gmail.com>
2023-04-04 17:00:30 -0300

    Criado Navbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,15 @@
+import Navbar from "../src/components/layout/Navbar"
+
 function HomePage () {
   
   return (
-    <div>
-      SejaDev
-    </div>
+    <>
+      <Navbar />
+      <div>
+        SejaDev
+      </div>
+    </>
+    
   )
 }
 
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledNavbar = styled.div `
+    background-color: ${props => props.theme.white};
+    height: 80px;
+    display: flex;
+    align-itens: center;
+    padding: 0 100px;
+`
+
+const StyledLogo = styled.span`
+    flex: 1;
+    font-weight: bold;
+    font-size: 20px;
+
+`
+
+
+function Navbar () {
+    return (
+        <StyledNavbar>
+            <StyledLogo># Social Dev </StyledLogo>
+            <div>
+                <Link href= "#">Desconectar</Link>
+            </div>
+
+        </StyledNavbar>
+    )
+
+}
+
+export default Navbar
