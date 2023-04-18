22c81b3d1a1f98429f5fa4b6a71955febc906c54
Gabriel Costa <22gabrielgc@gmail.com>
2023-04-18 15:18:20 -0300

    Criado rotas e service de posts

---
+++ modules/post/post.service.js
@@ -0,0 +1,17 @@
+import Post from './post.model'
+
+export const createPost = async (body, user) => {
+    return await Post.create({
+        text: body.text,
+        createdDate: new Date(),
+        createdBy: user.id
+    })
+}
+
+export const getPosts = async (limit = 10) => {
+    return await Post.find()
+        .populate('createdBy',  'user')
+        .sort({createdDate: -1})
+        .limit(limit)
+
+}
---
+++ pages/api/post/index.js
@@ -0,0 +1,33 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
+
+import createHandler from '../../../lib/middlewares/nextConnect'
+import validation from "../../../lib/middlewares/validation"
+import { ironConfig } from '../../../lib/middlewares/ironSession'
+
+import { createPostSchema } from '../../../modules/post/post.schema'
+import { createPost, getPosts } from '../../../modules/post/post.service'
+
+const handler = createHandler()
+    .post(validation({body: createPostSchema}), async (req, res) => {
+        try {
+            if (!req.session.user) return res.status(401).send()
+
+            const newPost = await createPost(req.body, req.session.user)
+            res.status(201).send(newPost)
+        } catch (err) {
+            return res.status(500).send(err.message)
+        }
+    })
+
+    .get(async (req, res) => {
+        try {
+            if (!req.session.user) return res.status(401).send()
+
+            const posts = await getPosts()
+            res.status(200).send(posts)
+        } catch (err) {
+            return res.status(500).send(err.message)
+        }
+    })
+
+export default withIronSessionApiRoute(handler, ironConfig)
